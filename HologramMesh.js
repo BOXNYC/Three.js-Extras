a61a1dba9c01cb8c2337e214d0d233491bdd20f9
Joseph James Weitzel <joe@box.biz>
2021-01-26 19:55:40 -0500

    Create HologramMesh.js

---
+++ HologramMesh.js
@@ -0,0 +1,16 @@
+THREE.HologramMesh = function( geometry, material ) {
+	
+	THREE.Mesh.call( this, geometry, material );
+	
+	this.rotateYToCamera = function( camera ){
+  	var meshPosition = new THREE.Vector3();
+    this.getWorldPosition( meshPosition );
+    var cameraPosition = new THREE.Vector3();
+    camera.getWorldPosition( cameraPosition );
+  	this.rotation.set( 0, Math.atan2( cameraPosition.x - meshPosition.x, cameraPosition.z - meshPosition.z ), 0 )
+	}
+	
+}
+
+THREE.HologramMesh.prototype = Object.create( THREE.Mesh.prototype );
+THREE.HologramMesh.prototype.isHologramMesh = true;
