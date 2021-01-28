6f93fe6dc0d7fb4076f74b8a1f7d4c2dba673a58
Joseph James Weitzel <joe@box.biz>
2021-01-28 16:48:42 -0500

    Update HologramGeometry.js

--- HologramGeometry.js
+++ HologramGeometry.js
@@ -1,17 +1,17 @@
 
 THREE.HologramGeometry = function( width, height, feetOffset, widthSegments, heightSegments ) {
-	
-	THREE.PlaneGeometry.call( this, width, height, widthSegments, heightSegments );
-	
-	var _feetOffset = feetOffset || new THREE.Vector2();
-	var _width = width;
-	var _height = height;
-	
-	Object.defineProperty( this, 'feetOffset', {
+  
+  THREE.PlaneGeometry.call( this, width, height, widthSegments, heightSegments );
+  
+  var _feetOffset = feetOffset || new THREE.Vector2();
+  var _width = width;
+  var _height = height;
+  
+  Object.defineProperty( this, 'feetOffset', {
     set: function( value ) {
       _feetOffset = value
       var matrix = new THREE.Matrix4().makeTranslation( ( _width * -( _feetOffset.x / 2 ) ), ( _height * 0.5 ) - ( _height * _feetOffset.y ), 0 )
-    	this.applyMatrix4( matrix );
+      this.applyMatrix4( matrix );
     },
     get: function() {
       return _feetOffset;
@@ -19,7 +19,7 @@ THREE.HologramGeometry = function( width, height, feetOffset, widthSegments, hei
   });
   
   this.feetOffset = _feetOffset
-	
+  
 }
 
 THREE.HologramGeometry.prototype = Object.create( THREE.PlaneGeometry.prototype );
