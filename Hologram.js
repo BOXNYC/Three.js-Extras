9a3f573172243282be9e0ccbbe500eac17417e2c
Joseph James Weitzel <joe@box.biz>
2021-01-28 16:48:03 -0500

    Update Hologram.js

--- Hologram.js
+++ Hologram.js
@@ -1,19 +1,23 @@
 
 THREE.Hologram = function( video, options ) {
-	
-	options = options || {
-  	// chromaKey, chromaRange, chromaMult, initVideoScale, feetOffset, widthSegments, heightSegments
-	};
-	if ( options.chromaRange ) options.range = options.chromaRange;
-	if ( options.chromaMult ) options.mult = options.chromaMult;
-	delete options.chromaRange;
-	delete options.chromaMult;
-	
-	options.initVideoScale = options.initVideoScale || 0.01;
-	
-	var scope = this;
-	
-  function loadeddata(e) {
+  
+  var scope = this;
+  
+  options = options || {
+    // chromaKey, chromaRange, chromaMult,
+    // initVideoScale, feetOffset, widthSegments, heightSegments,
+    // sillouetteShadow, sillouetteShadowOpacity,
+    // blurShadow, blurShadowOpacity **TODO
+  };
+  if ( options.chromaRange ) options.range = options.chromaRange;
+  if ( options.chromaMult ) options.mult = options.chromaMult;
+  delete options.chromaRange;
+  delete options.chromaMult;
+  
+  options.initVideoScale = options.initVideoScale || 0.01;
+  options.sillouetteShadowOpacity = options.sillouetteShadowOpacity || 0.4;
+  
+  function loadeddata() {
     
     scope.material = new THREE.ChromaKeyVideoShaderMaterial( video, options );
     scope.geometry = new THREE.HologramGeometry(
@@ -24,12 +28,24 @@ THREE.Hologram = function( video, options ) {
       options.heightSegments
     );
     
-  }
+    if ( options.sillouetteShadow ) {
+      var hologramShadow = new THREE.Mesh(
+        scope.geometry,
+        new THREE.ChromaKeyVideoShaderMaterialShadow( scope.material.map, options )
+      );
+      hologramShadow.rotateX( THREE.Math.degToRad( -90 ) )
+      scope.add( hologramShadow );
+    }
+    
+  };
   
   video.addEventListener( 'loadeddata', loadeddata );
-	
-	THREE.HologramMesh.call( this );
-	
+  if ( video.videoWidth ) loadeddata.call( video )
+  
+  THREE.HologramMesh.call( this );
+  
+  this.renderOrder = 1;
+  
 }
 
 THREE.Hologram.prototype = Object.create( THREE.HologramMesh.prototype );
